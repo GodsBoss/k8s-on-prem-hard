6008a5cc0f1c9e68f2d3d3c034c3ade01b45a1db
Timo Reitz <godsboss@gmx.de>
2019-01-15 20:00:56 +0100

    Wait a few seconds before creating roles so everything is up and running.

--- provision/rbac.sh
+++ provision/rbac.sh
@@ -2,5 +2,7 @@
 
 VAGRANT_HOME=/home/vagrant
 
+sleep 10 # ...
+
 kubectl apply -f ${VAGRANT_HOME}/kube-apiserver-to-kubelet-role.yaml
 kubectl apply -f ${VAGRANT_HOME}/kube-apiserver-to-kubelet-binding.yaml
